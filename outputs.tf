0470df2fd75e2f6a8a7e61176a96a04aae3897b3
Ashish Anand <DevOps.2285@gmail.com>
2025-08-03 14:16:09 +0530

    added additional code to output to display the IP's of the server

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "vpc_id" {
   description = "ID of the VPC"
   value       = aws_vpc.main_vpc.id
 }
+
+output "instance_names" {
+  description = "Name tags of all EC2 instances"
+  value       = [for instance in aws_instance.public_instances : instance.tags["Name"]]
+}
+
+output "instance_public_ips" {
+  description = "Public IPs of all EC2 instances"
+  value       = [for instance in aws_instance.public_instances : instance.public_ip]
+}
